705c1278944ba78a3d8b68e3c02fccf58b6bf971
sDauenbaugh <s.dauenbaugh@gmail.com>
2020-02-13 21:09:20 -0600

    added mail functionality

--- BlogApp/src/main/java/MailCron.java
+++ BlogApp/src/main/java/MailCron.java
@@ -1,8 +1,13 @@
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import javax.mail.*;
+import javax.mail.internet.*;
 
 public class MailCron extends HttpServlet {
 	
@@ -12,7 +17,22 @@ public class MailCron extends HttpServlet {
 		response.setContentType("text/plain");
 	    response.setCharacterEncoding("UTF-8");
 
-	    response.getWriter().print("Hello App Engine!\r\n");
+	    response.getWriter().print("You've got mail!\r\n");
+	    
+	    Properties props = new Properties();
+	    Session session = Session.getDefaultInstance(props, null);
+
+		try {
+			Message msg = new MimeMessage(session);
+			msg.setFrom(new InternetAddress("s.dauenbaugh@utexas.edu", "Sam School"));
+			msg.addRecipient(Message.RecipientType.TO, new InternetAddress("s.dauenbaugh@gmail.com", "Sam"));
+			msg.setSubject("Testing");
+			msg.setText("This message is a test");
+			Transport.send(msg);
+		} catch (AddressException e) {
+		} catch (MessagingException e) {
+		} catch (UnsupportedEncodingException e) {
+		}
 	}
 
 }
